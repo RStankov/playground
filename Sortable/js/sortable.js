fb2671781b076621eb803344adc03ae8cb99ee0a
RStankov <radoslavstankov@rstankov.local>
2009-06-21 19:11:04 +0300

    first working version of Sortable

--- Sortable/js/sortable.js
+++ Sortable/js/sortable.js
@@ -48,7 +48,9 @@ CD3.Dnd = {};
 		
 		element.makePositioned();
 		
-		element.fire('cd3:drag:start' /* todo: data */);
+		element.fire('cd3:drag:start', {
+			element: element
+		});
 				
 		document.observe('mousemove', move);
 		document.observe('mouseup', end);
@@ -75,7 +77,11 @@ CD3.Dnd = {};
 				element.scrollTo();
 		}
 */		
-		element.fire('cd3:drag:move' /* todo: data */);
+		element.fire('cd3:drag:move', {
+			x:		 e.pointerX(),
+			y:		 e.pointerY(),
+			element: element
+		});
 	}
 	
 	function end(e){
@@ -83,7 +89,9 @@ CD3.Dnd = {};
 		
 		e.stop();
 		
-		element.fire('cd3:drag:stop' /* todo: data */);
+		element.fire('cd3:drag:stop', {
+			element: element
+		});
 		element.setStyle(original);
 		
 		element = original = options = offset = null;
@@ -111,21 +119,59 @@ CD3.Dnd.Sortable = Class.create({
 		options		= Object.extend(this.constructor.defaultOptions, options || {});
 		
 		if (!options.handle){
-			options.handle = options.sort;
+			options.handle = options.item;
 		}
 		
 		this.container	= container;
 		this.options	= options;
 		
 		container.delegate(options.handle, 'mousedown', CD3.Dnd.startDragging);
+		container.observe('cd3:drag:start', this.onDragStart.bind(this));
+		container.observe('cd3:drag:move', this.onDrag.bind(this));
+		container.observe('cd3:drag:end', this.onDragEnd.bind(this));
+	},
+	onDragStart: function(e){
+		var handle  = e.memo.element,
+			options = this.options,
+			drag	= handle.match(options.item) ? handle : handle.up(options.item);
+		
+		this.changed = false;
+		this.drag 	 = drag;
+		this.items	 = this.container.select(options.list + ' ' + options.item).reject(function(item){ return item == drag });
+	},
+	onDrag: function(e){
+		var hover = this.items.find(function(item){
+			return Position.within(item, e.memo.x, e.memo.y);
+		});
+		
+		if (!hover) return;
+		
+		var overlap = Position.overlap('vertical', hover);
+		if (overlap > 0.5){
+			hover.insert({before: this.drag});
+		} else {
+			hover.insert({after: this.drag});
+		}
+		
+		this.changed = true;
+		this.container.observe('cd3:sort:changed', {
+			sortable: this
+		});
+	},
+	onDragEnd: function(e){
+		if (this.changed){
+			container.observe('cd3:sort:updated', {
+				sortable: this
+			});
+		}
+		
+		this.changed = this.drag = null = this.items;
 	}
 });
 
+// Sortable default options	
 CD3.Dnd.Sortable.defaultOptions = {
-	container:	'ul',
-	sort:		'li',
-	handle:		null,
-	onChange:	null,
-	onUpdate: 	null
-	// other dnd options
+	list:		'ul',
+	item:		'li',
+	handle:		null
 }
