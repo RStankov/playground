3ba4052819d6cb343ca7c131b43273b04e7a86dd
RStankov <rstankov@gmail.com>
2010-12-03 00:03:04 +0200

    add normal bind action

--- jQuery.on/on.js
+++ jQuery.on/on.js
@@ -1,5 +1,9 @@
 (function($){
   $.fn.on = function(eventName, selector, handler){
+    if (arguments.length == 2){
+      return this.bind(eventName, selector);
+    }
+
     return this.delegate(selector, eventName, handler);
   }
 })(jQuery);
--- jQuery.on/test.js
+++ jQuery.on/test.js
@@ -14,7 +14,14 @@ test("called with eventName(string), selector(string), handler(function)", 2, fu
 });
 
 test("called with eventName(string), hanlder(function)", function(){
-  pending();
+  var element   = $("#example-2"),
+      returened = element.on("click", function(){
+        ok(true, "this should be fired, by trigger click");
+      });
+
+  equal(element, returened, "jQuery#on should return jQuery instance");
+
+  element.trigger("click");
 });
 
 test("called with eventName(string), handles({ selector: handler })(object) ", function(){
