72b27a55dabda4ddb8d22e88e9c3ddba2169f6f0
RStankov <rstankov@gmail.com>
2010-12-03 00:10:43 +0200

    little clean up

--- jQuery.on/on.js
+++ jQuery.on/on.js
@@ -1,17 +1,17 @@
 (function($){
   $.fn.on = function(eventName, selector, handler){
-    if (arguments.length == 2){
-      if (!$.isPlainObject(selector)){
-        return this.bind(eventName, selector);
-      }
-
-      for(var rule in selector){
-        this.delegate(rule, eventName, selector[rule]);
-      }
-
-      return this;
+    if (arguments.length == 3){
+      return this.delegate(selector, eventName, handler);
     }
 
-    return this.delegate(selector, eventName, handler);
-  }
+    if (!$.isPlainObject(selector)){
+      return this.bind(eventName, selector);
+    }
+
+    for(var rule in selector){
+      this.delegate(rule, eventName, selector[rule]);
+    }
+
+    return this;
+  };
 })(jQuery);
