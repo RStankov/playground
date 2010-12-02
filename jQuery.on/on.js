b45763bf1402c8eaa741ade7b1a88b70c2497f50
RStankov <rstankov@gmail.com>
2010-12-03 00:24:48 +0200

    simplify on method by adding assignEvents function

--- jQuery.on/on.js
+++ jQuery.on/on.js
@@ -1,33 +1,29 @@
 (function($){
+  function assignEvents(eventName, rules){
+    if ($.isFunction(rules)){
+      return this.bind(eventName, rules);
+    }
+
+    for(var rule in rules){
+      this.delegate(rule, eventName, rules[rule]);
+    }
+
+    return this;
+  }
+
   $.fn.on = function(eventName, selector, handler){
     if (arguments.length == 3){
       return this.delegate(selector, eventName, handler);
     }
 
     if (arguments.length == 2){
-       if (!$.isPlainObject(selector)){
-          return this.bind(eventName, selector);
-        }
-
-        for(var rule in selector){
-          this.delegate(rule, eventName, selector[rule]);
-        }
-
-        return this;
+      return assignEvents.call(this, eventName, selector);
     }
 
-    if (arguments.length == 1){
-      for(var event in eventName){
-        if ($.isFunction(eventName[event])){
-          this.bind(event, eventName[event]);
-        } else {
-          for(var rule in eventName[event]){
-            this.delegate(rule, event, eventName[event][rule]);
-          }
-        }
-      }
-
-      return this;
+    for(var event in eventName){
+      assignEvents.call(this, event, eventName[event]);
     }
+
+    return this;
   };
 })(jQuery);
