f48ac396a53d0db137903159c50c3b364ec08045
RStankov <rstankov@gmail.com>
2010-12-02 23:39:12 +0200

    add the basic tests

---
+++ jQuery.on/test.js
@@ -0,0 +1,19 @@
+function pending(message){
+    ok(false, message || "not implemented yet");
+}
+
+test("called with eventName(string), selector(string), handler(function)", function(){
+  pending();
+});
+
+test("called with eventName(string), hanlder(function)", function(){
+  pending();
+});
+
+test("called with eventName(string), handles({ selector: handler })(object) ", function(){
+  pending();
+});
+
+test("called with { eventName : handles }(object)", function(){
+  pending();
+});
