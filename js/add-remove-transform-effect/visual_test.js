0906b7a15653f57d2d9184084d88bd856862cfe0
Radoslav Stankov <rstankov@gmail.com>
2011-07-17 15:20:45 +0300

    Extract finding of the component to separate function

--- js/add-remove-transform-effect/visual_test.js
+++ js/add-remove-transform-effect/visual_test.js
@@ -5,23 +5,27 @@ $(function() {
     $($('script[type="text/template"]').html()).tfx('insert', 'into', '[data-component="container"]');
   });
 
-  jDoc.delegate('[data-remove]', 'click', function() {
-    $(this).closest('[data-component="item"]').tfx('remove');
-  });
+  jDoc.delegate('[data-remove]', 'click', withItem(function() {
+    this.tfx('remove');
+  }));
 
-  jDoc.delegate('[data-show="form"]', 'click', function() {
+  jDoc.delegate('[data-show="form"]', 'click', withItem(function() {
     replace.call(this, 'body', 'form');
-  });
+  }));
 
-  jDoc.delegate('[data-show="body"]', 'click', function() {
+  jDoc.delegate('[data-show="body"]', 'click', withItem(function() {
     replace.call(this, 'form', 'body');
-  });
+  }));
+
+  function withItem(callback) {
+    return function(e) {
+      return callback.call($(this).closest('[data-component="item"]'), e);
+    };
+  }
 
   function replace(from, to) {
-    var item = $(this).closest('[data-component="item"]');
-
-    from = item.find('[data-component="' + from + '"]');
-    to = item.find('[data-component="' + to + '"]');
+    from = this.find('[data-component="' + from + '"]');
+    to = this.find('[data-component="' + to + '"]');
 
     from.tfx('replaceWith', to);
   }
