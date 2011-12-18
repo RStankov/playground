ec112d98db005e570d095d2408bcf3bcf1c4519a
Radoslav Stankov <rstankov@gmail.com>
2011-12-18 16:37:37 +0200

    Fix the boilerplate app.rb

--- boilerplate/app.rb
+++ boilerplate/app.rb
@@ -38,15 +38,15 @@ class AssetPipeline < Sprockets::Environment
 end
 
 Sass::Engine::DEFAULT_OPTIONS[:load_paths].tap do |load_paths|
-  load_paths << "#{root_path}/assets/stylesheets"
+  load_paths << "#{ROOT_PATH}/assets/stylesheets"
   load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"
 end
 
 builder = Rack::Builder.new do
   use Rack::CommonLogger
 
-  map '/assets' { run AssetPipeline.new(ROOT_PATH) }
-  map '/'       { run SlimTemplate.new(ROOT_PATH)  }
+  map('/assets') { run AssetPipeline.new(ROOT_PATH) }
+  map('/')       { run SlimTemplate.new(ROOT_PATH)  }
 end
 
 Rack::Handler::Mongrel.run builder, :Port => 3000
