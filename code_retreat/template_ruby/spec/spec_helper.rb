fcbeb2580d58e4a791a2fc4809e08ab7a4800b55
Radoslav Stankov <rstankov@gmail.com>
2012-07-20 23:31:42 +0300

    Get the ruby code retreat template up to date

--- code_retreat/template_ruby/spec/spec_helper.rb
+++ code_retreat/template_ruby/spec/spec_helper.rb
@@ -5,6 +5,6 @@ require "rspec"
 require File.join(File.dirname(__FILE__), '..', 'lib', 'game_of_life')
 require "support/matchers"
 
-Rspec.configure do |config|
+RSpec.configure do |config|
   config.include SpecMatchers
 end
