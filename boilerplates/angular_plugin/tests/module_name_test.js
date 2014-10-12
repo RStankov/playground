3f63383bdf221ec13ed9df905aadf5cb2a0e9562
Radoslav Stankov <rstankov@gmail.com>
2014-10-12 18:51:05 +0300

    Fully implement angular loadModule

--- boilerplates/angular_plugin/tests/module_name_test.js
+++ boilerplates/angular_plugin/tests/module_name_test.js
@@ -4,8 +4,9 @@ function loadModule(module, name) {
   name || (name = module);
 
   var loaded;
-  angular.injector(['ng', module], false).invoke(function() {
-  });
+  angular.injector(['ng', module], false).invoke([name, function(module) {
+    loaded = module;
+  }]);
 
   return loaded;
 }
